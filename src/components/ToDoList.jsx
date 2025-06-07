d1ee4a283aaa93f33fdd6321d93875974d33b21d
ShivamMishraa25 <shivam.m4464@gmail.com>
2025-06-07 16:14:59 +0530

    added edit option

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -6,17 +6,15 @@ function ToDoList() {
     const [val, setVal] = useState("");
     const [taskArr, setTaskArr] = useState(tasks);
     const [isEditing, setIsEditing] = useState(false);
+    const [editingId, setEditingId] = useState(null);
 
     function handleChange(e) {
         setVal(e.target.value);
     }
     function handleClick() {
-        if(val=="") {
+        if(val.trim()=="") {
             alert("Please enter a task");
             return;
-        }
-        if(isEditing) {
-
         }
         const newTask = {
             id: taskArr.length+1,
@@ -35,17 +33,38 @@ function ToDoList() {
         setTaskArr(deletedArr);
     }
     function editTask(id) {
+        setEditingId(id);
         const task = taskArr.find(task=>task.id==id);
         setVal(task.task);
         setIsEditing(true);
-        
+    }
+    function handleEdit() {
+        const editedArr = taskArr.map(task=>{
+            if(task.id==editingId) {
+                const obj = {
+                    id: task.id,
+                    task: val,
+                    isComplete: false
+                }
+                return obj;
+            }
+            return task;
+        })
+        setTaskArr(editedArr);
+        setVal("");
+        setIsEditing(false);
+        setEditingId(null);
     }
 
   return (
     <>
         <section className='addSection'>
             <input onKeyDown={handleKey} onChange={handleChange} value={val} className='addInput'></input>
-            <button onClick={handleClick} className='addBtn'>{isEditing?"Save Changes":"Add a Task"}</button>
+            {
+                isEditing?
+                <button onClick={handleEdit} className='addBtn'>Save Changes</button>:
+                <button onClick={handleClick} className='editBtn'>Add New Task</button>
+            }
         </section>
         <section className='ListSection'>
             {taskArr.map((task)=>{
